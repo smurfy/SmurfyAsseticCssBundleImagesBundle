8c98a69ee3d1dabecaa4b1f3c8308f19e7889a51
Philipp Andreas <philipp.andreas@vanzoo.de>
2015-10-13 16:58:39 +0000

    Support for more than one render engine at once (twig and php)

--- DependencyInjection/Compiler/TemplatingPass.php
+++ DependencyInjection/Compiler/TemplatingPass.php
@@ -11,6 +11,7 @@ namespace Smurfy\AsseticCssBundleImagesBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Reference;
 
 class TemplatingPass implements CompilerPassInterface
@@ -23,24 +24,30 @@ class TemplatingPass implements CompilerPassInterface
 
         $engines = $container->getParameterBag()->resolveValue($container->getParameter('templating.engines'));
 
-        $ourResource = $container->getDefinition('smurfy.assetic.css_resource');
+        $assetManager = $container->getDefinition('assetic.asset_manager');
 
         if (in_array('twig', $engines)) {
+            $twigCssResource = new DefinitionDecorator('smurfy.assetic.css_resource');
             $services = array();
             foreach ($container->findTaggedServiceIds('assetic.templating.twig') as $id => $attr) {
                 $services[] = new Reference($id);
             }
-            $ourResource->addMethodCall('setTemplates', array($services));
-            $ourResource->addMethodCall('setEngine', array( new Reference('assetic.twig_formula_loader')));
+            $twigCssResource->addMethodCall('setTemplates', array($services));
+            $twigCssResource->addMethodCall('setEngine', array( new Reference('assetic.twig_formula_loader')));
+            $container->setDefinition('smurfy.assetic.css_resource.twig', $twigCssResource);
+            $assetManager->addMethodCall('addResource', array(new Reference('smurfy.assetic.css_resource.twig'), 'cssbundleimages'));
         }
 
         if (in_array('php', $engines)) {
+            $phpCssResource = new DefinitionDecorator('smurfy.assetic.css_resource');
             $services = array();
             foreach ($container->findTaggedServiceIds('assetic.templating.php') as $id => $attr) {
                 $services[] = new Reference($id);
             }
-            $ourResource->addMethodCall('setTemplates', array($services));
-            $ourResource->addMethodCall('setEngine', array( new Reference('assetic.php_formula_loader')));
+            $phpCssResource->addMethodCall('setTemplates', array($services));
+            $phpCssResource->addMethodCall('setEngine', array( new Reference('assetic.php_formula_loader')));
+            $container->setDefinition('smurfy.assetic.css_resource.php', $phpCssResource);
+            $assetManager->addMethodCall('addResource', array(new Reference('smurfy.assetic.css_resource.php'), 'cssbundleimages'));
         }
     }
 }
